37a83c9b6204295a1be8a8120bb86732a3c214f6
show151 <136795114+show151@users.noreply.github.com>
2025-09-30 23:05:24 +0900

    add method

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -23,47 +23,7 @@ class Calculate
             double result = 0;
             string record = "";
 
-            if (op == "+")
-            {
-                result = num1 + num2;
-            }
-            else if (op == "-")
-            {
-                result = num1 - num2;
-            }
-            else if (op == "*")
-            {
-                result = num1 * num2;
-            }
-            else if (op == "/")
-            {
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                else
-                {
-                    Console.WriteLine("エラー: 0で割ることはできません。");
-                    return;
-                }
-            }
-            else if (op == "%")
-            {
-                if (num2 != 0)
-                {
-                    result = num1 % num2;
-                }
-                else
-                {
-                    Console.WriteLine("エラー: 0で割ることはできません。");
-                    return;
-                }
-            }
-            else
-            {
-                Console.WriteLine("エラー: 無効な演算子です。");
-                return;
-            }
+            result = Calc(num1, num2, op);
 
             record = $"{num1} {op} {num2} = {result}";
             Console.WriteLine($"結果: {record}");
@@ -73,7 +33,9 @@ class Calculate
             Console.Write("続けますか？ (exitで終了、履歴表示は'history'と入力): ");
             string ans = Console.ReadLine();
             if (ans == "exit")
+            {
                 break;
+            }
             else if (ans == "history")
             {
                 Console.WriteLine("==== 計算履歴 ====");
@@ -82,6 +44,51 @@ class Calculate
                     Console.WriteLine(h);
                 }
             }
+        }
+    }
+
+    static double Calc(double num1, double num2, string op)
+    {
+        if (op == "+")
+        {
+            return num1 + num2;
+        }
+        else if (op == "-")
+        {
+            return num1 - num2;
+        }
+        else if (op == "*")
+        {
+            return num1 * num2;
+        }
+        else if (op == "/")
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("エラー: 0で割ることはできません。");
+                return 0;
+            }
+        }
+        else if (op == "%")
+        {
+            if (num2 != 0)
+            {
+                return num1 % num2;
+            }
+            else
+            {
+                Console.WriteLine("エラー: 0で割ることはできません。");
+                return 0;
+            }
+        }
+        else
+        {
+            Console.WriteLine("エラー: 無効な演算子です。");
+            return 0;
         }
     }
 }
